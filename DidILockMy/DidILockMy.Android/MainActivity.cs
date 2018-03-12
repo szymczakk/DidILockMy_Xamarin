b0ba1ceb79c1dab555a4a7406780145084a8e7c7
szymczakk <rakaz666@gmail.com>
2018-03-12 21:03:26 +0100

    Initial setup

---
+++ DidILockMy/DidILockMy.Android/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace DidILockMy.Droid
+{
+    [Activity(Label = "DidILockMy", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+
+            AppCenter.Start("49b2583c-e7db-45dd-a57e-5d8ccbae3230", typeof(Analytics), typeof(Crashes));
+        }
+    }
+}
+
